ec07c80ff4d14b99fa71876cbd5aad00ddd75bac
Forrest Pruitt <fpruitt0922@gmail.com>
2015-01-24 17:06:45 -0500

    Adding falling block section

---
+++ Assets/Scripts/FallingBlockController.js
@@ -0,0 +1,49 @@
+﻿#pragma strict
+var tone: AudioClip;
+var scriptName : GameObject;
+var objectEnabled : boolean;
+
+function Start () {
+	objectEnabled=true;
+}
+
+function Update () {
+	if(rigidbody.velocity.magnitude < 7 && objectEnabled == true)
+	{
+        rigidbody.AddForce(Vector3.forward * -40);
+	}
+}
+
+function OnCollisionEnter(collision : Collision)
+{
+	if(collision.collider.tag == "Player")
+	{
+		renderer.material.color = Color.red;
+		var zValue : int = -7 +GameObject.Find("FallingBlockSpawner").GetComponent(FallingBlockSpawner).getCubesScored();
+		Debug.Log(zValue);
+		GetComponentInParent(Transform).transform.rigidbody.isKinematic=true;
+		GetComponentInParent(Transform).transform.position = Vector3(9,.5, zValue);
+		GetComponentInParent(Transform).transform.rotation = Quaternion.identity;
+		objectEnabled=false;
+		scriptName.GetComponent(FallingBlockSpawner).playNextNote();
+		yield WaitForSeconds (3);
+		scriptName.GetComponent(FallingBlockSpawner).spawnBlock();
+	}
+	
+	if(collision.collider.tag == "BackWall")
+	{
+		Debug.Log("Hit the backwall.");
+
+		if(objectEnabled==true)
+		{
+			// Start Over Here
+			Debug.Log("Hit the backwall.");
+			GetComponentInParent(Transform).transform.position = Vector3(100,.3,-7.9);
+	 		yield WaitForSeconds (3);
+ 			scriptName.GetComponent(FallingBlockSpawner).spawnBlock();
+			Destroy(this);
+		}
+	
+	}
+	
+}
---
+++ Assets/Scripts/puzzleStartScript.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+var scriptName : GameObject;
+var zoomedOut : boolean;
+function Start () {
+zoomedOut=false;
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(collider : Collider)
+{
+	if(collider.tag == "Player" || collider.tag == "PushBlock")
+	{
+		renderer.material.color = Color.green;
+		if(zoomedOut == false)
+		{
+			for(var i: int = 0; i < 10; i++)
+			{
+				Camera.main.orthographicSize +=1;
+				yield WaitForSeconds (0.001);
+			}
+			zoomedOut=true;
+			// Activate Falling Block Puzzle
+			GameObject.Find("FallingBlockSpawner").GetComponent(FallingBlockSpawner).spawnBlock();
+		}
+		
+
+
+		
+	}
+	
+}
